aee1d909288f0b21c9a8281f70dde267d68e5d80
Sweet Heaven <dalverq@gmail.com>
2021-02-23 11:05:28 +0300

    route fix

--- pages/tests/[title].js
+++ pages/tests/[title].js
@@ -1,15 +1,12 @@
 import {useRouter} from 'next/router'
-import { tests } from '../../data/tests';
 
 const TestPage = () => {
-
-  const test = props.test;
   const router = useRouter();
   const {title} = router.query;
 
   return (
     <div>
-      <p>I am {test.title} Page!</p>
+      <p>I am {title} Test Page!</p>
     </div>
   )
 }
